8eb7f119ab552eacdc8fa55205e4f722d57b4ec2
vaibhavippili <74439874+vaibhavippili@users.noreply.github.com>
2021-01-02 21:18:34 -0800

    node backend with news + about routes

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+const axios = require('axios')
+
+app.use(express.json());
+app.use(cors())
 const port = 3001
 
 
 
 const aboutus = [ 
     {'names': 'Vaibhav ', 'description': "Hi My name is Vaibhav!"},
-    {'names': 'Anoushka ', 'description': "Hi My name is Anoushka!"},
-    {'names': 'Joshua ', 'description': "Hi My name is Joshua!"}]
+    {'names': 'Anoushka ', 'description': "Hi My name is Anoushka!"}]
 
- 
+
 
 app.get('/about', (req, res) => {
-    req.headers['Content-Type'] = 'application/json'
-    req.headers['Accept'] = 'application.json'
     res.send(aboutus); 
 });
 
+app.get('/news', (req, res) => {
+    axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=fc522e115d6c438c9488caa5d83a3264')
+    .then((news, err) => {
+
+        if (err) res.send(err)
+        res.send(news.data)
+    })
+});
+
 app.listen(port, () => console.log(`Listening on Port ${port}`));
+
+
+
